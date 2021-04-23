87c976412f6e73b99068aac34d288fdee244021b
avbravo <avbravo@avbravo>
2021-04-23 10:57:09 -0500

    0.75

---
+++ src/main/resources/META-INF/resources/jmoordbjsf/hamburger.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
